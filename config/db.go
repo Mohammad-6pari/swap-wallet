ef129200208c04805aaf821e49472b421ffb8a8d
mohammad.sheshpari <mohammad.sheshpari@tapsi.cab>
2024-08-23 17:16:14 +0330

    config file for database with sample .env file added

---
+++ config/db.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+}
+
+func LoadConfig() Config {
+	return Config{
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+	}
+}
+
+func (c Config) GetDBConnStr() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		c.DBHost, c.DBPort, c.DBUser, c.DBPassword, c.DBName)
+}
